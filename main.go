ac52a97741d781a6e59f9db5317c1496c1846a69
EngKhallo <engkhalid.ict@gmail.com>
2023-10-20 20:53:02 +0300

    mongodb - connection

--- main.go
+++ main.go
@@ -1,8 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	// "github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
 
 func main() {
-	r := gin.Default()
+	// r := gin.Default()
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	var err error
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// check connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
 
 }
