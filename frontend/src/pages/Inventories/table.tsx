24e0f354140f229e80587a3be13b84c1394a5bbd
EngKhallo <engkhalid.ict@gmail.com>
2023-10-22 18:29:16 +0300

    Inventory Table

---
+++ frontend/src/pages/Inventories/table.tsx
@@ -0,0 +1,93 @@
+import { Table, Thead, Tr, Th, Tbody, Td, Flex, Text, Icon, Checkbox, TableContainer,Button } from "@chakra-ui/react";
+import { FiEdit2, FiTrash } from "react-icons/fi";
+import { Inventory } from "../../interfaces";
+
+interface Props {
+    inventory: Inventory[];
+    error: string;
+    loading: boolean;
+    onDelete: (inventory: Inventory) => void;
+}
+
+export const DataTable = ({ inventory, onDelete }: Props) => {
+    return (
+        <TableContainer maxW="100%" overflowX="scroll">
+            <Table variant="simple">
+                <Thead>
+                    <Tr>
+                        <Th>
+                            <Checkbox></Checkbox>
+                        </Th>
+                        <Th>Product Name</Th>
+                        <Th>Expire Date</Th>
+                        <Th>Cost Price</Th>
+                        <Th>Selling Price</Th>
+                        <Th>Currency</Th>
+                        <Th>Description</Th>
+                        <Th>Product Image</Th>
+                        <Th>Actions</Th>
+                    </Tr>
+                </Thead>
+                <Tbody textAlign="center">
+                    {(
+                        inventory.map((item) => (
+                            <Tr key={item._id}>
+                                <Td>
+                                    <Checkbox></Checkbox>
+                                </Td>
+                                <Td>
+                                    <Flex align="center">
+                                        <Text fontWeight="medium" ml={2}>
+                                            {item.product_name}
+                                        </Text>
+                                    </Flex>
+                                </Td>
+                                <Td>
+                                    <Text fontWeight="light" ml={2}>
+                                        {item.expire_date}
+                                    </Text>
+                                </Td>
+                                <Td>
+                                    <Text fontWeight="light" ml={2}>
+                                        {item.cost_price}
+                                    </Text>
+                                </Td>
+                                <Td>
+                                    <Text fontWeight="light" ml={2}>
+                                        {item.selling_price}
+                                    </Text>
+                                </Td>
+                                <Td>
+                                    <Text textAlign="center" fontWeight="light" ml={2}>
+                                        {item.currency}
+                                    </Text>
+                                </Td>
+                                <Td>
+                                    <Text textAlign="center" fontWeight="light" ml={2}>
+                                        {item.description}
+                                    </Text>
+                                </Td>
+                                <Td>
+                                    <Text fontWeight="light" ml={2}>
+                                        {item.product_image}
+                                    </Text>
+                                </Td>
+                                <Td>
+                                    <Flex justifyContent="space-around">
+                                        <Button variant="unstyled" bgColor="none">
+                                            <Icon color="orange" as={FiEdit2} />
+                                        </Button>
+                                        <Button variant="unstyled" bgColor="none" onClick={() => onDelete(item)}>
+                                            <Icon color="red" as={FiTrash} />
+                                        </Button>
+
+                                    </Flex>
+                                </Td>
+                            </Tr>
+                        ))
+                    )}
+                </Tbody>
+            </Table>
+        </TableContainer>
+    );
+};
