4abb79e908ebc35b62e80ba939f2f996480e03f3
EngKhallo <engkhalid.ict@gmail.com>
2023-10-24 22:08:36 +0300

    sales service api

---
+++ frontend/src/services/sales_service.ts
@@ -0,0 +1,15 @@
+import { Inventory, ProductSale, Sale} from "../interfaces";
+import apiClient from "./api-client";
+
+class SalesService {
+  getAllSales() {
+    const controller = new AbortController();
+    const request = apiClient.get<ProductSale[]>("/sales", {
+      signal: controller.signal,
+    });
+
+    return { request, cancel: () => controller.abort() };
+  }
+}
+
+export default new SalesService();
