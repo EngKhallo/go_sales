6fbf1adf1ea6d93535435538fdea1257be2360a5
EngKhallo <engkhalid.ict@gmail.com>
2023-10-29 20:25:22 +0300

    change URL from Local to deployed URL

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token") || "";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://mighty-beach-12110-f811d7687343.herokuapp.com/",
   headers: {
     "Content-type": "application/json",
     "Authorization": token,
