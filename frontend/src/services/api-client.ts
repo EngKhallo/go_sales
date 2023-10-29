a1861d7f20ef4ab68b6b2a2e5367d11e5d14c004
EngKhallo <engkhalid.ict@gmail.com>
2023-10-29 19:30:59 +0300

    api client - with token header

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+const token = localStorage.getItem("token") || "";
+
 export default axios.create({
-    baseURL: 'http://localhost:8080/',
-})
+  baseURL: "http://localhost:8080/",
+  headers: {
+    "Content-type": "application/json",
+    "Authorization": token,
+  },
+});
